c05b8e405d1f9bfcb521a39acdaf60045d0849f0
Manoj Kumar Shah <manojkumarshah111@gmail.com>
2023-11-09 00:45:16 +0530

    Changed gitignore to push the theme files Created a custom Wordpress page template with Nandan Global as template name Called default header and footer

---
+++ wp-content/themes/twentytwentyfour/nandan-global.php
@@ -0,0 +1,19 @@
+<?php
+
+// This is a custom page template for Nandan Global WordPress task
+
+/* Template Name: Nandan Global */
+
+// including the header of the theme
+
+get_header();
+
+// This is code area for Nandan Global WordPress task
+
+
+
+
+// incluiding the footer of the theme
+
+get_footer();
+?>
